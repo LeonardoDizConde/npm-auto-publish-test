eff946e29179083fbac1ade5750eb0665ea69adb
Leonardo de Jesus Diz Conde <leonardo.conde@simpli.com.br>
2023-08-28 14:37:07 -0300

    aaa

--- packages/npm-auto-publish-test-mono-repo-2/src/index.ts
+++ packages/npm-auto-publish-test-mono-repo-2/src/index.ts
@@ -1,2 +1,13 @@
-console.log('Hello world!fdfdfdfdf')
-console.log('Hello world!fdfdfdfdf')
+function generateRandomCode(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomCode = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomCode += characters.charAt(randomIndex);
+  }
+  return randomCode;
+}
+
+const codeLength = 10;
+const randomCode = generateRandomCode(codeLength);
+console.log(`Random code: ${randomCode}`);
